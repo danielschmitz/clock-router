804b650015f3b68afc55fb5bdad2207df1d0de02
Daniel Schmitz <danieljfa@gmail.com>
2024-03-03 16:40:55 -0300

    config to github actions

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    basePath: "/clock-router",
+    output: "export",
+    reactStrictMode: true,
+  };
+  
+  module.exports = nextConfig;
